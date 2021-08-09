293a154189769f8a972373d25e1fa44b0cf67cd0
z0031wc <anusha.u2@target.com>
2021-08-09 18:28:45 +0530

    Merge changes

--- src/components/utilities/RequestForm.jsx
+++ src/components/utilities/RequestForm.jsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-import React from "react";
-=======
 import React, { useState } from "react";
->>>>>>> Stashed changes
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -15,10 +11,7 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
-<<<<<<< Updated upstream
-=======
 import axios from "axios";
->>>>>>> Stashed changes
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -40,10 +33,6 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   root: {
     flexGrow: 1,
   },
@@ -63,42 +52,31 @@ const useStyles = makeStyles((theme) => ({
     padding: "2%",
     alignItems: "center",
   },
-<<<<<<< Updated upstream
-=======
 
   // input: {
   //   display: "none",
   // },
->>>>>>> Stashed changes
 }));
 
 export default function RequestForm() {
   const classes = useStyles();
-<<<<<<< Updated upstream
-=======
   const [name, setname] = useState("Jack");
   const [description, setdescription] = useState("");
   const [requestid, setrequestid] = useState("REQ0073");
   const [requeststate, setrequeststate] = useState("Active");
 
   const [service, setservice] = React.useState("");
->>>>>>> Stashed changes
   const [impacted_service, setimpacted_service] = React.useState("");
   const handleChange = (event) => {
     setimpacted_service(event.target.value);
   };
-<<<<<<< Updated upstream
-=======
 
   const [location, setlocation] = React.useState("");
->>>>>>> Stashed changes
   const [request_location, setrequest_location] = React.useState("");
   const handleChanges = (event) => {
     setrequest_location(event.target.value);
   };
 
-<<<<<<< Updated upstream
-=======
   const fetchlocations = () => {
     axios
       .get("http://localhost:8080/locations")
@@ -166,7 +144,6 @@ export default function RequestForm() {
       });
   };
 
->>>>>>> Stashed changes
   return (
     <div className={classes.root}>
       <div className={classes.mainContent}>
